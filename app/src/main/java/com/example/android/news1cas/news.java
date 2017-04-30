9d4d9c8a540c9e1edc902ded8777988159f80881
kowa1ski <kowalski@gmx.es>
2017-04-30 21:12:24 +0200

    limpieza de código

--- app/src/main/java/com/example/android/news1cas/news.java
+++ app/src/main/java/com/example/android/news1cas/news.java
@@ -1,17 +1,62 @@
 package com.example.android.news1cas;
 
 /**
- * An {@link News} object contains information of each news and hyperlink to webpage.
+ * An {@link News} object contains information of each new and hyperlink to webpage.
  */
 
 public class News {
 
-    //** information about tittle of new */
+    /**
+     * information about tittle of new
+     */
+    private String mTittle;
 
+    /**
+     * information about section
+     */
+    private String mSection;
 
+    /**
+     * Website URL of the new
+     */
+    private String mUrl;
 
+    /**
+     * Constructs a new {@link News} object.
+     *
+     * @param tittle  is tittle of the new ,
+     * @param section is the section into new ,
+     * @param url     is the website URL to find more details about the new .
+     */
 
+    public News(String tittle, String section, String url) {
 
+        mTittle = tittle;
+        mSection = section;
+        mUrl = url;
+
+    }
+
+    /**
+     * Returns tittle.
+     */
+    public String getmTittle() {
+        return mTittle;
+    }
+
+    /**
+     * Returns section.
+     */
+    public String getmSection() {
+        return mSection;
+    }
+
+    /**
+     * Returns the website URL to find more information about the new.
+     */
+    public String getmUrl() {
+        return mUrl;
+    }
 
 
 }
