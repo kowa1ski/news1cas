99f9c50271e3a4fd59ee67dfcb415dd06048c21d
kowa1ski <kowalski@gmx.es>
2017-05-04 10:41:53 +0200

    Creado vacío el método fetchNewsDATA(en QueryUtils). Ahora a rellenarlo

--- app/src/main/java/com/example/android/news1cas/QueryUtils.java
+++ app/src/main/java/com/example/android/news1cas/QueryUtils.java
@@ -1,10 +1,17 @@
 package com.example.android.news1cas;
 
+import java.util.List;
+
 /**
  * Created by Usuario on 04/05/2017.
  */
 
 public final class QueryUtils {
+
+    public static List<News> fetchNewsDATA(String requestUrl) {
+
+
+    }
 
 
 }
