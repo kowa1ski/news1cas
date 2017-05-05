1defaa6d56aa48e692779040c0e394b6ddea2c2b
kowa1ski <kowalski@gmx.es>
2017-05-05 07:43:33 +0200

    Completado el QueryUtil sólo hasta la línea 97, vamos lo que se ve de código. Hay que terminarlo en la siguiente sesión obviamente

--- app/src/main/java/com/example/android/news1cas/QueryUtils.java
+++ app/src/main/java/com/example/android/news1cas/QueryUtils.java
@@ -1,6 +1,11 @@
 package com.example.android.news1cas;
 
+import android.util.Log;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
@@ -10,13 +15,19 @@ import java.util.List;
 
 public final class QueryUtils {
 
+    private static final String TAG = "QueryUtils";
+
     public static List<News> fetchNewsDATA(String requestUrl) {
         // Create URL object
         URL url = createURL(requestUrl);
 
         // Perform HTTP request to the URL and receive a JSON response back
         String jsonResponse = null;
-        jsonResponse = makeHttpRequest(url);
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(TAG, "Problem making the HTTP requesst.", e);
+        }
 
         // Extract relevant fields from the JSON response and create a list of
         //  {@Link new}
@@ -32,12 +43,56 @@ public final class QueryUtils {
      */
     private static URL createURL(String stringUrl){
         URL url = null;
-        //TODO . It´s needed a MalformedURLException?
-        url = new URL(stringUrl);
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Problem building the URL ", e);
+        }
 
-
+        return url;
     }
 
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        //If the URL is null, then return early
+        if(url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConection = null;
+        InputStream inputStream = null;
+        try {
+            urlConection = (HttpURLConnection) url.openConnection();
+            urlConection.setReadTimeout(10000);
+            urlConection.setConnectTimeout(15000);
+            urlConection.setRequestMethod("GET");
+            urlConection.connect();
+
+            //If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConection.getResponseCode() == 200) {
+                inputStream = urlConection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(TAG, "Error response code: " + urlConection.getResponseCode());
+            }
+
+        } catch (IOException e) {
+            Log.e(TAG, "Problem retrieving the news JSON results.", e);
+        } finally {
+            if (urlConection != null) {
+                urlConection.disconnect();
+            }
+            if (inputStream != null) {
+                //Closing the input stram could throw an IOException, which is why
+                //the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
 
 }
 
