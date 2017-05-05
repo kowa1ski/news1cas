fcb16480855b3b079bc8ce2bccbad9c428e46073
kowa1ski <kowalski@gmx.es>
2017-05-05 12:13:03 +0200

    Ya falta poco. Ponte a seguir los pasos del TODO de la línea 146

--- app/src/main/java/com/example/android/news1cas/QueryUtils.java
+++ app/src/main/java/com/example/android/news1cas/QueryUtils.java
@@ -1,12 +1,20 @@
 package com.example.android.news1cas;
 
+import android.text.TextUtils;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -94,7 +102,83 @@ public final class QueryUtils {
         return jsonResponse;
     }
 
+    /**
+     * Convert the {@Link InputStream} into a String whit contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    /**
+     * Return a lis of {@Link News} objects thath has been built up from
+     * parsing the given JSON response.
+     */
+    private static List<News> extractFeatureFromJson(String newsJSON) {
+        // If JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(newsJSON)) {
+            return null;
+        }
+        //Create an empty ArrayList that we can start adding news to
+        List<News> newsArrayList = new ArrayList<>();
+
+        //Try parse the JSON response string. If there´s a problem with the way the JSON
+        // is formatted, a JSONException exception object will be trhown.
+        //Catch the exception so the app doesn´t crash, and print the error message to the logs.
+        try {
+
+            //Create a JSONObject from the JSON response string
+            JSONObject baseJsonResponse = new JSONObject(newsJSON);
+
+            /**
+             * El siguiente bloque va a extraer la información de las
+             * diferentes keys del objeto JSON.
+             * TODO - los pasos son
+             * 1. carga la url que tengo en comentarios en el main.
+             * 2. identifica las claves que nos van a hacer falta.
+             * 3. ponte a extraer esas claves debidamente.
+             */
+            //Extract the JSONArray associated with the key called
+
+
+
+
+
+        } catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" bloc,
+            // catch the exception here, so the app doesn´t crash. Print a log message
+            // with the message from the exception.
+            Log.e("QueryUtils", "Problem parsing the news JSON results", e);
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
